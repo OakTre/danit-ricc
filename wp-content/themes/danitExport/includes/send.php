85b05df2c271a0858e4dcbd59c485a4715928dec
OakTre <nailgalimov8@gmail.com>
2022-10-19 21:32:23 +0300

    upd: send form

--- wp-content/themes/danitExport/includes/send.php
+++ wp-content/themes/danitExport/includes/send.php
@@ -8,41 +8,64 @@
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
+$token = '5784895850:AAGGZSro2uXroSYdSHyPTSiuqz309XTVzgs';
+$chat_id = '-719954746';
 $name = $_POST["name"];
 $company = $_POST["companay"];
 $text = $_POST["text"];
 $tel = $_POST["tel"];
 $mail = $_POST["mail"];
 
-$queryUrl = 'https://ricc-it.bitrix24.ru/rest/18/44l42tj7bfzqjbl7/crm.lead.add.json';
-$queryData = http_build_query(array(
-    'fields' => array(
-        'TITLE' => 'Форма экспорта c сайта danit.ea',
-        'NAME' => $name,
-        'PHONE' => array(
-            array(
-                "VALUE" => $tel,
-                "VALUE_TYPE" => "WORK"
-            )
-        ),
-        'EMAIL' => array(
-            array(
-                "VALUE" => $mail
-            )
-        ),
-        'COMPANY_TITLE' => $company,
-        'COMMENTS' => $text,
-    ),
-    'params' => array("REGISTER_SONET_EVENT" => "Y")
-));
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_SSL_VERIFYPEER => 0,
-    CURLOPT_POST => 1,
-    CURLOPT_HEADER => 0,
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $queryUrl,
-    CURLOPT_POSTFIELDS => $queryData,
-));
-$result = curl_exec($curl);
-curl_close($curl);
+$arr = array(
+    'Форма:' => "Заявка с danit.ae",
+    'Имя:' => $name,
+    'Телефон:' => $tel,
+    'Почта:' => $mail,
+    'Сообщение:' => $text,
+    'Наименование компании:' => $company,
+);
+
+foreach ($arr as $key => $value) {
+    $message .= "<b>" . $key . "</b> " . $value . "%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}", "r");
+
+if ($sendToTelegram) {
+    echo "OK";
+} else {
+    echo "Error";
+}
+
+// $queryUrl = 'https://ricc-it.bitrix24.ru/rest/18/44l42tj7bfzqjbl7/crm.lead.add.json';
+// $queryData = http_build_query(array(
+//     'fields' => array(
+//         'TITLE' => 'Форма экспорта c сайта danit.ea',
+//         'NAME' => $name,
+//         'PHONE' => array(
+//             array(
+//                 "VALUE" => $tel,
+//                 "VALUE_TYPE" => "WORK"
+//             )
+//         ),
+//         'EMAIL' => array(
+//             array(
+//                 "VALUE" => $mail
+//             )
+//         ),
+//         'COMPANY_TITLE' => $company,
+//         'COMMENTS' => $text,
+//     ),
+//     'params' => array("REGISTER_SONET_EVENT" => "Y")
+// ));
+// $curl = curl_init();
+// curl_setopt_array($curl, array(
+//     CURLOPT_SSL_VERIFYPEER => 0,
+//     CURLOPT_POST => 1,
+//     CURLOPT_HEADER => 0,
+//     CURLOPT_RETURNTRANSFER => 1,
+//     CURLOPT_URL => $queryUrl,
+//     CURLOPT_POSTFIELDS => $queryData,
+// ));
+// $result = curl_exec($curl);
+// curl_close($curl);
